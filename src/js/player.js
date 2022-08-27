47ff9e6be86f0057942cd5236eccda46390f31f4
tsukumi <tsukumijima@users.noreply.github.com>
2022-08-27 16:46:40 +0900

    update: adjust play buffer during syncing

--- src/js/player.js
+++ src/js/player.js
@@ -179,7 +179,7 @@ class DPlayer {
      */
     sync(quiet = false) {
         if (this.options.live) {
-            const time = utils.getVideoDuration(this.video, this.template) - 0.7; // 0.7s is play buffer
+            const time = utils.getVideoDuration(this.video, this.template) - 0.6; // 0.6s is play buffer
             try {
                 this.video.currentTime = time;
             } catch (error) {
