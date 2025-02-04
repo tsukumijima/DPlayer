21974927cc7a2e3571d8bc723c6ec21ad1ceae97
tsukumi <tsukumijima@users.noreply.github.com>
2025-02-04 22:19:14 +0900

    chore: release v1.32.0

--- dist/d.ts/thumbnails.d.ts
+++ dist/d.ts/thumbnails.d.ts
@@ -3,11 +3,21 @@ declare class Thumbnails {
     container: HTMLElement;
     barWidth: number;
     events: Events;
+    private width;
+    private height;
+    private totalCount;
+    private columnCount;
     constructor(options: {
         container: HTMLElement;
         barWidth: number;
         url: string;
         events: Events;
+        duration?: number;
+        interval?: number;
+        totalCount?: number;
+        width?: number;
+        height?: number;
+        columnCount?: number;
     });
     resize(width: number, height: number, barWrapWidth: number): void;
     show(): void;
